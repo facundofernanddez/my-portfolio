820cb9f80ce5e891c4d0014606c000c0fe5a2a0c
Facundo Fernandez <ff96_@hotmail.com>
2022-11-27 11:47:30 -0300

    commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,14 +12,34 @@ export const HomeCard = () => {
   );
 };
 
+export const IndividualProyectCard = () => {
+  return (
+    <div className="card mt-3">
+      <img src="" class="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
+};
+
 export const ProjectsCard = () => {
   return (
     <div className="container mt-5">
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">Proyectos</h5>
-          <p className="card-text">Aqui mis proyectos realizados</p>
         </div>
+        <IndividualProyectCard />
+        <IndividualProyectCard />
+        <IndividualProyectCard />
       </div>
     </div>
   );
